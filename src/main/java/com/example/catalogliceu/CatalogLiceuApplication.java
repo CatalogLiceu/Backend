e10cd2956d792aca5a7b2917ac512d68884f4384
bionsp <utvictor0@gmail.com>
2024-08-08 14:08:25 +0300

    Initial commit.

---
+++ src/main/java/com/example/catalogliceu/CatalogLiceuApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogliceu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogLiceuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogLiceuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catalogliceu/CatalogLiceuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalogliceu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogLiceuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
