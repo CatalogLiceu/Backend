14883ac01dbca98e2529267e69c1627e5834aac8
bionsp <utvictor0@gmail.com>
2024-08-10 00:33:40 +0300

    Service pentru AnClasa.

---
+++ src/main/java/com/example/catalogliceu/service/AnClasaService.java
@@ -0,0 +1,29 @@
+package com.example.catalogliceu.service;
+
+import com.example.catalogliceu.entities.AnClasa;
+import com.example.catalogliceu.repositories.AnClasaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnClasaService {
+    private final AnClasaRepository anClasaRepository;
+    public AnClasaService(AnClasaRepository anClasaRepository) {
+        this.anClasaRepository = anClasaRepository;
+    }
+    public void adaugaAn(Long nrClasa) {
+        AnClasa anClasa = AnClasa.builder()
+                .nrClasa(nrClasa)
+                .build();
+        anClasaRepository.save(anClasa);
+    }
+    public void stergeAn(Long nrClasa) {
+        if(anClasaRepository.findById(nrClasa).isPresent()) {
+            anClasaRepository.delete(anClasaRepository.findById(nrClasa).get());
+        }
+    }
+    public void stergeAn(AnClasa anClasa) {
+        if(anClasaRepository.findById(anClasa.getId()).isPresent()) {
+            anClasaRepository.delete(anClasaRepository.findById(anClasa.getId()).get());
+        }
+    }
+}
